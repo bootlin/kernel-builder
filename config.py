0acaebe5942652291faa41f9ccc558b9be10a65f
Florent Jacquet <florent.jacquet@free-electrons.com>
2017-07-28 16:27:42 +0200

    config: don't build tcrypt defconfig on mvebu-backports
    Signed-off-by: Florent Jacquet <florent.jacquet@free-electrons.com>

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ WHITE_LIST = {
         "arm/mvebu_v7_defconfig+tcrypt": [
             "mainline/master",
             "next/master",
-            "mvebu-backports/4.12-rc6_backports",
             ],
         "arm64/defconfig": [
             "mvebu-backports/4.12-rc6_backports",
